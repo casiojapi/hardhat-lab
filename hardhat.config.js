8dbbae711701d8cc692fe1c09500452002a013bf
maximo santoro <maximo.santoro@iovlabs.org>
2022-11-29 16:57:57 -0300

    set up network in hardhat (yes i know the seedphrase ok is just for testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,22 @@
+const fs = require('fs');
+
+const rskTestnetSeedPhrase = fs
+  .readFileSync('.rsk-testnet-seed-phrase')
+  .toString()
+  .trim();
+
+if (!rskTestnetSeedPhrase || rskTestnetSeedPhrase.split(' ').length != 12) {
+  throw new Error("seed phrase is not valid (bip-39)")
+}
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.13",
+  networks: {
+    hardhat: {},
+    rskregtest: {
+      url: 'http://localhost:4444'
+    },
+  },
 };
